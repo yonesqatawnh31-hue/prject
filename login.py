bf2808600b34b02020138cc61bdc47c32b5e578d
Ynes Al Qatawnh <yonesqatawnh31@gmail.com>
2026-04-19 14:46:32 +0300

    added login log

---
+++ login.py
@@ -0,0 +1,7 @@
+username = input("Enter Username: ")
+password = input("Enter Password: ")
+
+if username == "yones" and password == "2026":
+    print("Access Granted. Welcome to the Secure Network.")
+else:
+    print("Access Denied. Verification Failed.") 
